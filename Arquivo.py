6e0ba9bd1026bf00531631d5d3a2d0cde9a9739a
aluno <aluno>
2019-08-23 10:35:23 -0300

    alteracoes linha

--- Arquivo.py
+++ Arquivo.py
@@ -1,5 +1,13 @@
 class Arquivo(object):
 
-    with open("teste.txt", "r") as file:
-        for line in file:
-            print(line)
+    def imprimir(self):
+        with open("teste.txt", "r") as file:
+            for line in file:
+             self.checkLine(line)
+
+
+    def checkLine(linha):
+        if not linha.__str__().startswith("/n") and not linha.__str__().startswith("//"):
+          if linha.__str__().find("//"):
+            x = linha.__str__().split("//")
+            print(x[0])
