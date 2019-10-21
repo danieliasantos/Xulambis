583b2592fb6c8ec9518c326f794ad422e55c17b7
Daniel Elias <danieliasantos.dev@protonmail.com>
2019-10-20 23:37:12 -0300

    Analise Lexica

--- src/AnaliseLexica.py
+++ src/AnaliseLexica.py
@@ -4,40 +4,42 @@ import re
 import platform
 
 if platform.sys.platform.__contains__("win"):
+    from TabelaSimbolos import TabelaSimbolos
     from Token import Token
 else:
+    from src.TabelaSimbolos import TabelaSimbolos
     from src.Token import Token
 
 class AnaliseLexica(object):
-    __hashTokens = {
-        "int": Token("int", "int"),
-        "float": Token("float", "float"),
-        "bool": Token("bool", "bool"),
-        "double": Token("double", "double"),
-        "true": Token("double", "double"),
-        "false": Token(),
-        "if": Token(),
-        "while": Token(),
-        "break": Token(),
-        "{": Token(),
-        "}": Token(),
-        "(": Token(),
-        ")": Token(),
-        "[": Token(),
-        "]": Token(),
-        ";": Token(),
-        "=": Token(),
-        "+": Token(),
-        "<": Token(),
-        ">": Token(),
-        ">=": Token(),
-        "<=": Token(),
-        "!=": Token(),
-        "==": Token()
-    }
-    __regex = re.compile('[{]|([a-z{A-Z]+[\s]+(([a-zA-Z]+[\s]*[\;])|(([a-zA-Z]+[\s]+)*[\=]+[\s]+(([0-9]+([\.][0-9]+)*['
-                         '\;])|([a-zA-Z]+[\s]*[\;])|(([a-zA-Z]+|[0-9]+([\.][0-9]+)*)[\s]*([\+]|[\-]|[\*]|[\/])[\s]*(['
-                         'a-zA-Z]+|[0-9]+([\.][0-9]+)*)[\;])))))|([a-zA-Z]+[\s]+[\(]+[\s](([a-zA-Z]+[\s][\)])|([a-zA-Z]+['
-                         '\s]([<]|[>]|[>=]|[<=]|[!=]|[==])[\s]([a-zA-Z]+|[0-9]+)[\s][\)]))[\s]([\{]|[a-zA-Z]+[\;]))|(['
-                         'a-zA-Z]+[\;])|[\}]')
 
+    __listTokens = []
+    __listaCodigo = []
+
+    def __init__(self, lista):
+        __regex = re.compile('[{]|'
+                             '([a-zA-Z]+[\s]+(([a-zA-Z]+[\s]*[\;])|(([a-zA-Z]+[\s]+)*[\=]+[\s]+(([0-9]+([\.][0-9]+)*[\;]*)|'
+                             '([a-zA-Z]+[\s]*[\;])|(([a-zA-Z]+|[0-9]+([\.][0-9]+)*)[\s]*([\+]|[\-]|[\*]|[\/])[\s]*([a-zA-Z]+|[0-9]+([\.][0-9]+)*)[\;]*)))))|'
+                             '([a-zA-Z]+[\s]+[\(]+[\s](([a-zA-Z]+[\s][\)])|([a-zA-Z]+[\s]([<]|[>]|[>=]|[<=]|[!=]|[==])[\s]([a-zA-Z]+|[0-9]+)[\s][\)]))[\s]([\{]|[a-zA-Z]+[\;]*))|'
+                             '([a-zA-Z]+[\;]*)|'
+                             '[\}]')
+        if len(lista) > 0:
+            self.__listaCodigo = lista
+
+    def regexTest(self, linha):
+        self.__regex.match(linha)
+
+    def analyseLexems(self):
+        tokens = TabelaSimbolos()
+        for i in self.__listaCodigo:
+            line = i
+            v = line.__str__().split(';')
+            for j in v:
+                l = j
+                if self.regexTest(l):
+                    if tokens.lexExists(l):
+                        self.__listTokens.append(l)
+                    else:
+                        self.__listTokens.append(Token('id', l))
+
+    def printTokens(self):
+        print(self.__listTokens)
--- src/TabelaSimbolos.py
+++ src/TabelaSimbolos.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+if platform.sys.platform.__contains__("win"):
+    from Token import Token
+else:
+    from src.Token import Token
+
 class TabelaSimbolos(object):
-    __hashSymbols = [
-        "int",
-        "float",
-        "bool",
-        "double",
-        "true",
-        "false",
-        "if",
-        "while",
-        "break",
-        "{",
-        "}",
-        "(",
-        ")",
-        "[",
-        "]",
-        ";",
-        "=",
-        "+",
-        "<",
-        ">",
-        ">=",
-        "<=",
-        "!=",
-        "==",
-    ]
+
+    __hashTokens = {
+        "int": Token('int', 'keyword'),
+        "float": Token('float', 'keyword'),
+        "bool": Token('bool', 'keyword'),
+        "double": Token('double', 'keyword'),
+        "string": Token('string', 'keyword'),
+        "true": Token('double', 'keyword'),
+        "false": Token('false', 'keyword'),
+        "if": Token('if', 'keyword'),
+        "else": Token('else', 'keyword'),
+        "while": Token('while', 'keyword'),
+        "break": Token('break', 'keyword'),
+        "{": Token('{', 'delimiter'),
+        "}": Token('{', 'delimiter'),
+        "(": Token('{', 'delimiter'),
+        ")": Token('{', 'delimiter'),
+        "[": Token('{', 'delimiter'),
+        "]": Token('{', 'delimiter'),
+        ";": Token('{', 'delimiter'),
+        "=": Token('=', 'operation'),
+        "+": Token('+', 'operation'),
+        "-": Token('-', 'operation'),
+        "*": Token('*', 'operation'),
+        "/": Token('/', 'operation'),
+        "<": Token('<', 'logical'),
+        ">": Token('>', 'logical'),
+        ">=": Token('>=', 'logical'),
+        "<=": Token('<=', 'logical'),
+        "!=": Token('!=', 'logical'),
+        "==": Token('==', 'logical')
+    }
 
     def lexExists (self, lexema):
         return lexema in self.__hashSymbols
+
+    def getToken(self, lexema):
+        return self.__hashTokens[lexema]
